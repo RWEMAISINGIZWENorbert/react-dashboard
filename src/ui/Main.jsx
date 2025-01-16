641e94ec04ef0aff549bfb86e0acf2f58a39e4e9
rwema <rwema916@gmail.com>
2025-01-16 19:59:49 +0200

    build

--- src/ui/Main.jsx
+++ src/ui/Main.jsx
@@ -1,7 +1,10 @@
 const Main = ({children}) => {
   return (
-    <div className="text-gray-500 bg-gray-100 p-4  sm:ml-2 flex gap-2 flex-col lg:flex-row translate-all duration-200 mt-14 dark:bg-gray-800">{children}</div>
+    <div className="text-gray-500 bg-gray-100 p-4 sm:ml-2 flex gap-2 flex-col lg:flex-row translate-all duration-200 mt-14 dark:bg-gray-800">{children}</div>
   )
 }
 
-export default Main
+export default Main;
+// Main.Proptypes = {
+//    children: PropTypes.any;
+// }
